d1eeaea350fab697474797294db4d3c03f6e4fae
henrydall@softwarestrategies.io <henrydall@softwarestrategies.io>
2021-11-14 07:46:22 -0800

    Added Builder capacity

--- src/main/java/com/akatia/printmgr/data/PrintJobsRequest.java
+++ src/main/java/com/akatia/printmgr/data/PrintJobsRequest.java
@@ -3,6 +3,7 @@ package com.akatia.printmgr.data;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PrintJobsRequest {
