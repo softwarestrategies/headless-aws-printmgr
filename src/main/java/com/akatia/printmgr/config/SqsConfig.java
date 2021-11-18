50f56dfd2bb8c7503f1bcebbcdc60488a9d5c1dd
henrydall@softwarestrategies.io <henrydall@softwarestrategies.io>
2021-11-17 16:47:20 -0800

    Added SimpleMessageListenerContainerFactory bean.  It seems that without it, sometimes the AWS SQS library has connection issues.

--- src/main/java/com/akatia/printmgr/config/SqsConfig.java
+++ src/main/java/com/akatia/printmgr/config/SqsConfig.java
@@ -5,6 +5,7 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.messaging.config.SimpleMessageListenerContainerFactory;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,6 +24,14 @@ public class SqsConfig {
 
     @Value("${aws.secret-key}")
     private String secretKey;
+
+    @Bean
+    public SimpleMessageListenerContainerFactory simpleMessageListenerContainerFactory(AmazonSQSAsync amazonSQS){
+        SimpleMessageListenerContainerFactory factory = new SimpleMessageListenerContainerFactory();
+        factory.setAmazonSqs(amazonSQS);
+        factory.setMaxNumberOfMessages(10);
+        return factory;
+    }
 
     @Bean
     public QueueMessagingTemplate queueMessagingTemplate() {
